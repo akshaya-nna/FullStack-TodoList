329df0965bf9c6c4446577180973491db71782fd
akshaya-nna <nna.akshaya@gmail.com>
2018-06-13 14:51:10 -0400

    Completed Delete and Update

--- public/app.js
+++ public/app.js
@@ -7,6 +7,15 @@ $(document).ready(function () {
             createTodo();
         }
     })
+
+    $('.list').on('click','li',function () {
+        updateTodo($(this));
+    })
+
+    $('.list').on('click','span',function (event) {
+        event.stopPropagation();
+        removeTodo($(this).parent());
+    })
 });
 function addTodos(todos) {
     todos.forEach(function (todo) {
@@ -15,7 +24,9 @@ function addTodos(todos) {
 }
 
 function addTodo(todo) {
-    var newTodo=$('<li class="task">'+todo.name+'</li>');
+    var newTodo=$('<li class="task">'+todo.name+' <span>X</span></li>');
+    newTodo.data('id',todo._id);
+    newTodo.data('completed',todo.completed)
     if(todo.completed){
         newTodo.addClass("done");
     }
@@ -31,5 +42,37 @@ function createTodo() {
         })
         .catch(function (err) {
             console.log(err);
+        })
+}
+
+function removeTodo(todo) {
+    var clickedId=todo.data('id');
+    var deleteUrl='/api/todos/' +clickedId;
+
+    $.ajax({
+        method:'DELETE',
+        url:deleteUrl
+    })
+        .then(function (data) {
+        todo.remove();
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+}
+
+function updateTodo(todo) {
+    var updateUrl='/api/todos/'+todo.data('id');
+    var isDone=!todo.data('completed');
+    var updateData={completed:isDone}
+    $.ajax({
+        method:'PUT',
+        url:updateUrl,
+        data:updateData
+    })
+        .then(function (updatedTodo) {
+            todo.toggleClass("done");
+            todo.data('completed',isDone);
+
         })
 }
